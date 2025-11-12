079f88bb6700b37577eddee4ec2e56886905155f
Jarrett Tay <jarrett.tay.my@gmail.com>
2025-11-12 13:54:03 +0800

    Added internshipWithdrawalApplicants classes

---
+++ bin/InternshipWithdrawalApplicants.java
@@ -0,0 +1,32 @@
+public class InternshipWithdrawalApplicants {
+    private Student student;
+    private Internship internship;
+    private int id;
+    private static int counter = 0;
+    public InternshipWithdrawalApplicants(Student student, Internship internship) {
+        this.student = student;
+        this.internship = internship;
+        this.id = counter++;
+    }
+    // Getter method for student
+    public Student getStudent() {
+        return student;
+    }
+
+    // Getter method for internship
+    public Internship getInternship() {
+        return internship;
+    }
+    // Getter method for id
+    public int getId() {
+        return id;
+    }
+    // Method to withdraw internship.
+    public boolean withdrawInternship() {
+        if (student.getAppliedInternship() == null) {
+            return false;
+        }
+        student.withdrawAcceptedInternship();
+        return true;
+    }
+}
---
+++ bin/InternshipWithdrawalDbMgr.java
@@ -0,0 +1,96 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Comparator;
+import java.util.stream.Collectors;
+
+public class InternshipWithdrawalDbMgr {
+    private List<InternshipWithdrawalApplicants> internshipWithdrawalList;
+    private static InternshipWithdrawalDbMgr instance;
+    private InternshipWithdrawalDbMgr() {
+        internshipWithdrawalList = new ArrayList<InternshipWithdrawalApplicants>();
+    }
+
+    public static InternshipWithdrawalDbMgr getInstance() {
+        if (instance == null) {
+            instance = new InternshipWithdrawalDbMgr();
+        }
+        return instance;
+    }
+    // Getter methods for applicants
+    public InternshipWithdrawalApplicants get(int id) {
+        for (InternshipWithdrawalApplicants i: internshipWithdrawalList) {
+            if (i.getId() == id) {
+                return i;
+            }
+        }
+        return null;
+    }
+    // Add applicants based on student and internship. If student already inside, return false.
+    // Otherwise add applicants to list and return true.
+    public boolean add(Student student, Internship internship) {
+        for (InternshipWithdrawalApplicants i: internshipWithdrawalList) {
+            if (i.getStudent() == student) {
+                return false;
+            }
+        }
+        InternshipWithdrawalApplicants internshipWithdrawalApplicants = new InternshipWithdrawalApplicants(student, internship);
+        internshipWithdrawalList.add(internshipWithdrawalApplicants);
+        return true;
+    }
+    // Remove appplicants. Return true if applicants in list, then remove them.
+    // False if applicants not in list.
+    public boolean remove(int id) {
+        for (InternshipWithdrawalApplicants i: internshipWithdrawalList) {
+            if (i.getId() == id) {
+                internshipWithdrawalList.remove(get(id));
+                return true;
+            }
+        }
+        return false;
+    }
+    // Sort based in attributes. 
+    public List<InternshipWithdrawalApplicants> sort(InternshipWithdrawalApplicantsAttributes sortBy) {
+        switch (sortBy) {
+            case InternshipWithdrawalApplicantsAttributes.ID:
+                return internshipWithdrawalList
+                    .stream()
+                    .sorted(Comparator.comparing(x -> x.getId()))
+                    .collect(Collectors.toList());
+            case InternshipWithdrawalApplicantsAttributes.STUDENT:
+                return internshipWithdrawalList
+                    .stream()   
+                    .sorted(Comparator.comparing(x -> x.getStudent().getName()))
+                    .collect(Collectors.toList());
+            case InternshipWithdrawalApplicantsAttributes.INTERNSHIP:
+                return internshipWithdrawalList
+                    .stream()
+                    .sorted(Comparator.comparing(c -> c.getInternship().getTitle()))
+                    .collect(Collectors.toList());
+        }
+        return null;
+    }
+    // Filter based on attributes.
+    // For student, args comapares with the name,
+    // for internship, args compares with the title.
+    public List<InternshipWithdrawalApplicants> filter(InternshipWithdrawalApplicantsAttributes filterBy, String args) {
+        switch (filterBy) {
+            case InternshipWithdrawalApplicantsAttributes.ID:
+                return internshipWithdrawalList
+                    .stream()
+                    .filter(x -> ((Integer)x.getId()).toString().contains(args))
+                    .collect(Collectors.toList());
+            case InternshipWithdrawalApplicantsAttributes.STUDENT:
+                return internshipWithdrawalList
+                    .stream()   
+                    .filter(x -> x.getStudent().getName().contains(args))
+                    .collect(Collectors.toList());
+            case InternshipWithdrawalApplicantsAttributes.INTERNSHIP:
+                return internshipWithdrawalList
+                    .stream()
+                    .filter(c -> c.getInternship().getTitle().contains(args))
+                    .collect(Collectors.toList());
+        }
+        return null;
+    }
+        
+}
