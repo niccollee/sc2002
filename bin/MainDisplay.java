4ca1dd95faeb3490f7864f6a0578526a256201a5
niccollee <nicoleljx22@gmail.com>
2025-11-20 16:50:09 +0800

    ui changes

--- bin/MainDisplay.java
+++ bin/MainDisplay.java
@@ -9,14 +9,16 @@ public class MainDisplay extends ADisplay{
      */
     public void showMenu() {
         String output = """
-                =========================
+                ==========================================
                 Internship Management System
+                ==========================================
+                Log in as:
                 1)Student
                 2)Career Staff
                 3)Company Representative
                 4)Register as company representative
                 5)Quit
-                =========================
+                ==========================================
                 """;
         System.out.println(output);
     }
