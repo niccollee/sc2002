29525bb0655be1ddb8c5703e62b0f36a11ec6f8f
niccollee <nicoleljx22@gmail.com>
2025-11-20 14:14:00 +0800

    javadoc

--- bin/CompanyRepStatus.java
+++ bin/CompanyRepStatus.java
@@ -1,5 +1,12 @@
+/**
+ * Represents the approval status of a company representative.
+ * A representative can be approved, pending approval or rejected.
+ */
 public enum CompanyRepStatus {
+    /** The representative has been approved and can perform full actions. */
     APPROVED,
+    /** The representative's application is pending review. */
     PENDING,
+    /** The representative's application has been rejected. */
     REJECT
 }
